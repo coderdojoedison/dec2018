beb37e3201a476601948bddfe9e010f35b98371f
Coderdojo Edison <edison.us@coderdojo.com>
2018-12-08 16:15:42 +0000

    Create states_base.py

---
+++ states_base.py
@@ -0,0 +1,26 @@
+import kanren
+from kanren import run, fact, eq, Relation, var
+
+
+#read the file containing the coastal states
+##with open(file_coastal, 'r') as f:
+##    line = f.read()
+##    coastal_states = line.split(',')
+
+#add the info to the fact base
+##for state in coastal_states:
+##    fact(coastal, state.lower())
+
+#read the file containing the adjacent states
+
+##adjlist = []
+
+##with open(file_adjacent, 'r') as f:
+##    adjlist_raw = [line.strip().split(',') for line in f if line and
+##line[0].isalpha()]
+##    for adj in adjlist_raw:
+##        ll1 = []
+##        for adj1 in adj:
+##            ll1.append(adj1.lower())
+##        adjlist.append(ll1)
+                                        
