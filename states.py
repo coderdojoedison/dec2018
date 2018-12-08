d7dc0d6e1373265618d9f82a3d168558fe8f4826
Coderdojo Edison <edison.us@coderdojo.com>
2018-12-08 16:10:00 +0000

    Update states.py

--- states.py
+++ states.py
@@ -1,5 +1,5 @@
-import logpy
-from logpy import run, fact, eq, Relation, var
+import kanren
+from kanren import run, fact, eq, Relation, var
 
 adjacent = Relation()
 coastal = Relation()
@@ -10,25 +10,55 @@ file_adjacent = '/home/pi/december-2018/adjacent_states.txt'
 #read the file containing the coastal states
 with open(file_coastal, 'r') as f:
     line = f.read()
-    coastal_states = line.split(',')
+    c_states = line.split(',')
+    coastal_states = [state.lower() for state in c_states]
+
 #add the info to the fact base
 for state in coastal_states:
     fact(coastal, state)
 
 #read the file containing the adjacent states
-with open(file_adjacent, 'r') as f:
-    adjlist = [line.strip().split(',') for line in f if line and
-line[0].isalpha()]
 
+adjlist = []
+with open(file_adjacent, 'r') as f:
+    adjlist_raw = [line.strip().split(',') for line in f if line and
+line[0].isalpha()]
+    for adj in adjlist_raw:
+        ll1 = []
+        for adj1 in adj:
+            ll1.append(adj1.lower())
+        adjlist.append(ll1)
+                                          
 #add info to fact base
 for L in adjlist:
     head, tail = L[0], L[1:]
     for state in tail:
-        fact(adjacent, head, state)
+        fact(adjacent, head, state.lower())
 x = var()
 y = var() 
 
-output = run(0, x, adjacent('Oregon', x))
-print ('\nList of states adjacent to Oregon: ')
-for item in output: 
+
+state = input("Which state's adjacency do you want to analyze: ")
+l_state = state.lower()
+
+output = run(0, x, adjacent(l_state, x))
+print ('\nList of states adjacent to ' + state + ' : ')
+for item in output:
     print(item)
+
+state = input("If any exist, which coastal states are adjacent to this state: ")
+l_state = state.lower()
+output = run(0, x, adjacent(l_state, x) , coastal(x))
+print ('\nList of coastal states' )
+for item in output:
+    print(item)
+
+state = input("Enter a state name to determine if it's coastal: ")
+l_state = state.lower()
+#print("coastal states are <<", coastal_states, "<<")
+
+if l_state in coastal_states:
+    
+    print ('\n' + state + " is a coastal State")
+else:
+    print ('\n' + state + ' is not a coastal state') 
